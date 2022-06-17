53be5ce9d0114f0e537f62e0907fecb386c9ef0d
Jason Savelli <savellijason@gmail.com>
2022-06-17 13:52:30 +0200

    task: react query default config cache improvement

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,15 @@ import { Routes } from "./routes";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const App = () => {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        // The fetched data in this App aren't updated a lot, so we can assume that we want to cache them
+        staleTime: 60000 * 24,
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
 
   return (
     <div>
