ee3d06863a2bf1534351b1c78fc6e60db06cbcba
Jason Savelli <jsavelli@splio.com>
2024-12-26 15:27:36 +0100

    task: setup tanstack query

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,26 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Home } from "./home/home";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // Data we're fetching are really stable and should only be updated once in a year
+      // So we should use the maximum cache available
+      gcTime: Infinity,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export const App = () => {
-  return <Home />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Home />
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 };
